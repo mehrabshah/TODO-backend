431465de91aaea7f222c5bd917ea435d70ad19ca
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-01-05 15:25:30 +0500

    initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TaskModule } from './modules/task/task.module';
+import { ConfigModule } from '@nestjs/config';
+import mongoose from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    
+    ConfigModule.forRoot({
+        envFilePath:'.env',
+        isGlobal:true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+
+    TaskModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
